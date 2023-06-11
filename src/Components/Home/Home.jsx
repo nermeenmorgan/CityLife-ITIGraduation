0a1388c8caae2045309757ecf9fea8cafb33bfba
Marwaahmedfouad <marwa.fouad102@gmail.com>
2023-06-11 21:35:52 +0300

    Updating Home Component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <img className={`${styles.img} d-block w-100 `} src={myImage1} alt="First slide" />
-                            <div class="carousel-caption d-none d-md-block">
+                            <div className="carousel-caption d-none d-md-block">
                                 <h5>.......</h5>
                                 <p>...</p>
                             </div>
@@ -32,7 +32,7 @@ export default function Home() {
 
                         <div className="carousel-item">
                             <img className={`${styles.img} d-block w-100 `} src={myImage2} alt="First slide" />
-                            <div class="carousel-caption d-none d-md-block">
+                            <div className="carousel-caption d-none d-md-block">
                                 <h5>.......</h5>
                                 <p>...</p>
                             </div>
@@ -42,7 +42,7 @@ export default function Home() {
 
                         <div className="carousel-item ">
                             <img className={`${styles.img} d-block w-100 `} src={myImage2} alt="First slide" />
-                            <div class="carousel-caption d-none d-md-block">
+                            <div className="carousel-caption d-none d-md-block">
                                 <h5>.......</h5>
                                 <p>...</p>
                             </div>
@@ -95,45 +95,66 @@ export default function Home() {
                 <h5 className='text-center'>Services</h5>
                 <div className="container">
                     <div className="row m-3">
-                        <div className="col-md-4 ">
-                            <div className='p-3'>
-                                <div className={`${styles.dFlex}`} style={{ backgroundImage: `url(${img2})`, backgroundSize: "100% 100%", height: '200px' }}>
-                                    <Link className="p-3 text-white fs-3 " to="/ContactUs" style={{ textDecoration: 'none' }}>Banks</Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className='p-3'>
-                                <div className={`${styles.dFlex}`} style={{ backgroundImage: `url(${img4})`, backgroundSize: "100% 100%", height: '200px' }}>
-                                    <Link className="p-3 text-white fs-3 " to="/Markets" style={{ textDecoration: 'none' }}>Markets</Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className='p-3'>
-                                <div className={`${styles.dFlex}`} style={{ backgroundImage: `url(${img3})`, backgroundSize: "100% 100%", height: '200px' }}>
-                                    <Link className="p-3 text-white fs-3" to="/Hospital" style={{ textDecoration: 'none' }}>Hospital</Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className='p-3'>
-                                <div className={`${styles.dFlex}`} style={{ backgroundImage: `url(${img5})`, backgroundSize: "100% 100%", height: '200px' }}>
-                                    <Link className="p-3 text-white fs-3" to="/Education" style={{ textDecoration: 'none' }}>Education</Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className='p-3'>
-                                <div className={`${styles.dFlex}`} style={{ backgroundImage: `url(${img1})`, backgroundSize: "100% 100%", height: '200px' }}>
-                                    <Link className="p-3 text-white fs-3" to="/Cinema" style={{ textDecoration: 'none' }}>Cinema</Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
+                        
+                        <div className="col-md-6 col-lgcol-md-6 col-lg-4">
                             <div className='p-3 '>
-                                <div className={`${styles.dFlex} ${styles.font}` } style={{ backgroundImage: `url(${img6})`, backgroundSize: "100% 100%", height: '200px' }}>
-                                    <Link className="p-3 text-white fs-3" to="/MaintenancePayment" style={{ textDecoration: 'none' }}>Payment & Complain</Link>
+                                <div className={`${styles.dFlex} ${styles.font} position-relative`} style={{ backgroundImage: `url(${img2})`, backgroundSize: "100% 100%", height: '200px' }}>
+                                    <Link className="p-3 text-white fs-3 position-absolute top-0 bottom-0 start-0 end-0  " to="/banks" style={{ textDecoration: 'none' }}>
+                                        <h4 className='position-absolute top-50 mt-5'>Banks</h4>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6 col-lg-4">
+                            <div className='p-3 '>
+                                <div className={`${styles.dFlex} ${styles.font} position-relative`} style={{ backgroundImage: `url(${img4})`, backgroundSize: "100% 100%", height: '200px' }}>
+                                    <Link className="p-3 text-white fs-3 position-absolute top-0 bottom-0 start-0 end-0  " to="/markets" style={{ textDecoration: 'none' }}>
+                                        <h4 className='position-absolute top-50 mt-5'>Markets</h4>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6 col-lg-4">
+                            <div className='p-3 '>
+                                <div className={`${styles.dFlex} ${styles.font} position-relative`} style={{ backgroundImage: `url(${img3})`, backgroundSize: "100% 100%", height: '200px' }}>
+                                    <Link className="p-3 text-white fs-3 position-absolute top-0 bottom-0 start-0 end-0  " to="/hospitals" style={{ textDecoration: 'none' }}>
+                                        <h4 className='position-absolute top-50 mt-5'>Hospital</h4>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+                        <div className="col-md-6 col-lg-4">
+                            <div className='p-3 '>
+                                <div className={`${styles.dFlex} ${styles.font} position-relative`} style={{ backgroundImage: `url(${img5})`, backgroundSize: "100% 100%", height: '200px' }}>
+                                    <Link className="p-3 text-white fs-3 position-absolute top-0 bottom-0 start-0 end-0  " to="/Education" style={{ textDecoration: 'none' }}>
+                                        <h4 className='position-absolute top-50 mt-5'>Education</h4>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="col-md-6 col-lg-4">
+                            <div className='p-3 '>
+                                <div className={`${styles.dFlex} ${styles.font} position-relative`} style={{ backgroundImage: `url(${img1})`, backgroundSize: "100% 100%", height: '200px' }}>
+                                    <Link className="p-3 text-white fs-3 position-absolute top-0 bottom-0 start-0 end-0  " to="/cinema" style={{ textDecoration: 'none' }}>
+                                        <h4 className='position-absolute top-50 mt-5'>Cinema</h4>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="col-md-6 col-lg-4">
+                            <div className='p-3 '>
+                                <div className={`${styles.dFlex} ${styles.font} position-relative`} style={{ backgroundImage: `url(${img6})`, backgroundSize: "100% 100%", height: '200px' }}>
+                                    <Link className="p-3 text-white fs-3 position-absolute top-0 bottom-0 start-0 end-0  " to="/maintenance" style={{ textDecoration: 'none' }}>
+                                        <h4 className='position-absolute top-50 mt-5'>Payment & Complain</h4>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
@@ -144,57 +165,76 @@ export default function Home() {
 
 
 
+            
+
+            
             <section className=' '>
                 <div className='container p-3 '>
                     <h5 className='text-center py-2 '>Categories</h5>
                     <div className='row'>
-                        <div className='col-md-3 col-sm-2 text-center p-1 '>
-                            <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
-                                <i class="fa-solid fa-utensils fs-1  align-items-end  "></i>
-                                <div className=''>Restaurants</div>
-                            </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3  text-center p-1 '>
+                            <Link className='text-decoration-none text-dark' to={'/restaurants'}>
+                                <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
+                                    <i className="fa-solid fa-utensils fs-1"></i>
+                                    <div className=''>Restaurants</div>
+                                </div>
+                            </Link>
                         </div>
-                        <div className='col-md-3 col-sm-2 text-center p-1'>
-                            <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
-                                <i class="fa-solid fa-cart-shopping fs-1"></i>
-                                <div className=''>shopping</div>
-                            </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 text-center'>
+                            <Link className='text-decoration-none text-dark' to={'/shopping'}>
+                                <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
+                                    <i className="fa-solid fa-cart-shopping fs-1"></i>
+                                    <div className=''>Shopping</div>
+                                </div>
+                            </Link>
                         </div>
-                        <div className='col-md-3 col-sm-2 text-center p-1'>
-                            <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
-                                <i class="fa-solid fa-shirt fs-1"></i>
-                                <div className=''>Fashion</div>
-                            </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 text-center'>
+                            <Link className='text-decoration-none text-dark' to={'/fashion'}>
+                                <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
+                                    <i className="fa-solid fa-shirt fs-1"></i>
+                                    <div className=''>Fashion</div>
+                                </div>
+                            </Link>
                         </div>
-                        <div className='col-md-3 col-sm-2 text-center p-1'>
-                            <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
-                                <i class="fa-solid fa-heart-pulse fs-1"></i>
-                                <div className=''>Health</div>
-                            </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 text-center'>
+                            <Link className='text-decoration-none text-dark' to={'/health'}>
+                                <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
+                                    <i className="fa-solid fa-heart-pulse fs-1"></i>
+                                    <div className=''>Health</div>
+                                </div>
+                            </Link>
                         </div>
-                        <div className='col-md-3 col-sm-2 text-center p-1'>
-                            <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
-                                <i class="fa-solid fa-table-tennis-paddle-ball fs-1"></i>
-                                <div className=''>Sports</div>
-                            </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 text-center'>
+                            <Link className='text-decoration-none text-dark' to={'/sports'}>
+                                <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
+                                    <i className="fa-solid fa-table-tennis-paddle-ball fs-1"></i>
+                                    <div className=''>Sports</div>
+                                </div>
+                            </Link>
                         </div>
-                        <div className='col-md-3 col-sm-2 text-center p-1'>
-                            <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
-                                <i class="fa-solid fa-wrench fs-1"></i>
-                                <div className=''>Maintenance</div>
-                            </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 text-center'>
+                            <Link className='text-decoration-none text-dark' to={'/maintenance'}>
+                                <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
+                                    <i className="fa-solid fa-wrench fs-1"></i>
+                                    <div className=''>Maintenance</div>
+                                </div>
+                            </Link>
                         </div>
-                        <div className='col-md-3 col-sm-2 text-center p-1'>
-                            <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
-                                <i class="fa-solid fa-house-laptop fs-1"></i>
-                                <div className=''>Home Services</div>
-                            </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 text-center'>
+                            <Link className='text-decoration-none text-dark' to={'/homeServices'}>
+                                <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
+                                    <i className="fa-solid fa-house-laptop fs-1"></i>
+                                    <div className=''>Home Services</div>
+                                </div>
+                            </Link>
                         </div>
-                        <div className='col-md-3 col-sm-2 text-center '>
-                            <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
-                                <i class="fa-solid fa-truck-plane fs-1"></i>
-                                <div className=''>Transportation</div>
-                            </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 text-center '>
+                            <Link className='text-decoration-none text-dark' to={'/transportation'}>
+                                <div className='p-4 shadow  m-2 bg-white rounded w-75 mx-auto'>
+                                    <i className="fa-solid fa-truck-plane fs-1"></i>
+                                    <div className=''>Transportation</div>
+                                </div>
+                            </Link>
                         </div>
                     </div>
                 </div>
