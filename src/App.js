e1c1c7aa7a2c0df7782a94a4a94e75ce95ce11f7
nermeen <nermen.morgan99@gmail.com>
2023-06-11 13:15:41 +0200

    restaurants

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import ContactUs from './Components/ContactUs/ContactUs';
 import NotFound from './Components/NotFound/NotFound';
-<<<<<<< HEAD
-import Login from './Components/Login/Login';
-import Register from './Components/Register/Register';
-import Restaurants from './Components/Resturants/Resturants';
-=======
-import SignUp from './Components/SignUp/SignUp';
+import Login from './Components/Login/Login'; // renamed from SignUp
 import Restaurants from './Components/Restaurants/Restaurants';
->>>>>>> 463d078cb651af21a94a7e0518887d58565c9bbd
 import Data from './Context/Data';
 import Markets from './Components/Markets/Markets';
 import Banks from './Components/Banks/Banks';
@@ -27,29 +21,9 @@ import Maintenance from './Components/Maintenance/Maintenance';
 import HomeServices from './Components/HomeServices/HomeServices';
 import Sports from './Components/Sports/Sports';
 import './App.css';
-<<<<<<< HEAD
-import Shawarma from './Components/Resturants/shawarma';
-import Fried from './Components/Resturants/fried';
-import Fastfood from './Components/Resturants/fastfood';
-// import Seafood from './Components/Resturants/seafood';
-import Orientalfood from './Components/Resturants/orientalfood';
-import Seafood from './Components/Resturants/seafood';
-import RestaurantLayout from './Components/Resturants/Resturants';
-import Pizza from './Components/Resturants/pizza';
-import ResurantLayout from './Components/Resturants/Resturants';
-// const restaurantsRoutes = [
-//   { path: '', element: <Restaurants /> },
-//   { path: 'seafood', element: <Seafood /> },
-//   { path: 'fastfood', element: <Fastfood /> },
-//   { path: 'fried', element: <Fried /> },
-//   { path: 'shawarma', element: <Shawarma /> },
-//   { path: 'orientalfood', element: <Orientalfood /> },
-// ];
-=======
-import SignIn from './Components/SignIn/SignIn';
+import SignUp from './Components/SignUp/SignUp';
+import SignIn from './Components/SignIn/SignIn'; // added
 import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute';
->>>>>>> 463d078cb651af21a94a7e0518887d58565c9bbd
-
 
 function App() {
   let routers = createBrowserRouter([
@@ -58,44 +32,6 @@ function App() {
       element: <Layout />,
       children: [
         { index: true, element: <Home /> },
-<<<<<<< HEAD
-        { path: 'about', element: <About /> },
-        { path: 'stores', element: <Stores /> },
-        { path: 'markets', element: <Markets /> },
-        { path: 'hospitals', element: <Hospitals /> },
-        { path: 'education', element: <Education /> },
-        { path: 'cinema', element: <Cinema /> },
-        { path: 'payment', element: <MaintenancePayment /> },
-        {
-          path: "resturants",
-          element: <RestaurantLayout />,
-          children: [
-            { index: true, element: <Restaurants></Restaurants> },
-            { path: "fried", element: <Fried></Fried> },
-            { path: "fastfood", element: <Fastfood></Fastfood> },
-            { path: "oriental", element: <Orientalfood></Orientalfood> },
-            { path: "pizza", element: <Pizza></Pizza> },
-            { path: "seafood", element: <Seafood></Seafood> },
-            { path: "shawarma", element: <Shawarma></Shawarma> },
-            { path: "*", element: <NotFound></NotFound> },
-          ],
-        },
-// { path: "resturants/*", element: <ResurantLayout />}, // Update this line
-        { path: 'shopping', element: <Shopping /> },
-        { path: 'fashion', element: <Fashion /> },
-        { path: 'health', element: <Health /> },
-        { path: 'sports', element: <Sports /> },
-        { path: 'maintenance', element: <Maintenance /> },
-        { path: 'homeServices', element: <HomeServices /> },
-        { path: 'transportation', element: <Transportation /> },
-        { path: 'contactus', element: <ContactUs /> },
-        { path: 'login', element: <Login /> },
-        { path: 'register', element: <Register /> },
-        { path: '*', element: <NotFound /> },
-      ],
-    },
-  ]);
-=======
         { path: "about", element: <About /> },
         { path: "banks", element: <Banks /> },
         { path: "markets", element: <Markets /> },
@@ -112,13 +48,11 @@ function App() {
         { path: "homeServices", element: <HomeServices /> },
         { path: "transportation", element: <Transportation /> },
         { path: "contactus", element: <ContactUs /> },
-        { path: "signin", element: < SignIn /> },
-        { path: "signup", element: <SignUp /> },
+        { path: "signin", element: <SignIn /> },
+        { path: "signup", element: <SignUp /> }, // renamed from Login
         { path: "*", element: <NotFound /> }]
     }
   ])
-
->>>>>>> 463d078cb651af21a94a7e0518887d58565c9bbd
 
   return (
     <>
--- src/Context/Data.js
+++ src/Context/Data.js
@@ -2,101 +2,92 @@ import React, { createContext, useEffect } from 'react';
 import { useState } from 'react';
 import jwtDecode from 'jwt-decode';
 import axios from 'axios';
-<<<<<<< HEAD
-import { useEffect } from 'react';
-=======
->>>>>>> 463d078cb651af21a94a7e0518887d58565c9bbd
 
-
-<<<<<<< HEAD
-export default function Data({children}) {
-                                                 // Managing User Data
-    const [userData, setUserData] = useState(null);
-=======
 export const DataContext = createContext();
+
 export default function Data(props) {
-    const ImgsArr = [
-        "https://www.talaatmoustafa.com/Upload/75rehab%201.jpg",
-        "https://www.talaatmoustafa.com/Upload/8rehab%202.jpg",
-        "https://www.talaatmoustafa.com/Upload/72rehab%203.jpg",
-        "https://www.talaatmoustafa.com/Upload/27rehab%208.jpg",
-        "https://www.talaatmoustafa.com/Upload/4rehab%204.jpg",
-        "https://www.talaatmoustafa.com/Upload/21rehab%205.jpg",
-        "https://www.talaatmoustafa.com/Upload/16rehab%206.jpg",
-        "https://www.talaatmoustafa.com/Upload/10rehab%209.jpg",
-        "https://www.talaatmoustafa.com/Upload/80rehab%2010.jpg",
-        "https://www.talaatmoustafa.com/Upload/14rehab%2011.jpg",
-        "https://www.talaatmoustafa.com/Upload/23rehab%2015.jpg",
-        "https://www.talaatmoustafa.com/Upload/77rehab%2014.jpg",
-        "https://www.talaatmoustafa.com/Upload/54rehab%2013.jpg",
-        "https://www.talaatmoustafa.com/Upload/35rehab%2019.jpg",
-        "https://www.talaatmoustafa.com/Upload/15rehab%2017.jpg",
-        "https://www.talaatmoustafa.com/Upload/3rehab%2020.jpg",
-        "https://www.talaatmoustafa.com/Upload/85rehab%2018.jpg",
-        "https://www.talaatmoustafa.com/Upload/61rehab%2021.jpg",
-        "https://www.talaatmoustafa.com/Upload/41rehab%2023.jpg",
-        "https://www.talaatmoustafa.com/Upload/0rehab%2022.jpg"
-    ];
+  const ImgsArr = [
+    "https://www.talaatmoustafa.com/Upload/75rehab%201.jpg",
+    "https://www.talaatmoustafa.com/Upload/8rehab%202.jpg",
+    "https://www.talaatmoustafa.com/Upload/72rehab%203.jpg",
+    "https://www.talaatmoustafa.com/Upload/27rehab%208.jpg",
+    "https://www.talaatmoustafa.com/Upload/4rehab%204.jpg",
+    "https://www.talaatmoustafa.com/Upload/21rehab%205.jpg",
+    "https://www.talaatmoustafa.com/Upload/16rehab%206.jpg",
+    "https://www.talaatmoustafa.com/Upload/10rehab%209.jpg",
+    "https://www.talaatmoustafa.com/Upload/80rehab%2010.jpg",
+    "https://www.talaatmoustafa.com/Upload/14rehab%2011.jpg",
+    "https://www.talaatmoustafa.com/Upload/23rehab%2015.jpg",
+    "https://www.talaatmoustafa.com/Upload/77rehab%2014.jpg",
+    "https://www.talaatmoustafa.com/Upload/54rehab%2013.jpg",
+    "https://www.talaatmoustafa.com/Upload/35rehab%2019.jpg",
+    "https://www.talaatmoustafa.com/Upload/15rehab%2017.jpg",
+    "https://www.talaatmoustafa.com/Upload/3rehab%2020.jpg",
+    "https://www.talaatmoustafa.com/Upload/85rehab%2018.jpg",
+    "https://www.talaatmoustafa.com/Upload/61rehab%2021.jpg",
+    "https://www.talaatmoustafa.com/Upload/41rehab%2023.jpg",
+    "https://www.talaatmoustafa.com/Upload/0rehab%2022.jpg"
+  ];
 
-    const [movies,setMovies] = useState([])
->>>>>>> zz
-
-    useEffect(()=>{
-       axios.get('http://localhost:3005/cinema')
-       .then((res)=>{
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3005/cinema')
+      .then((res) => {
         setMovies(res.data)
-       })
-    },[])
-    const [userData, setUserData] = useState(null);
-    function saveUserData() {
-        let encodedToken = localStorage.getItem("userToken");
-        let decodedToken = jwtDecode(encodedToken);
-        setUserData(decodedToken);
-    }
-                                                // Restaurant Data Management
-    const[AllRestaurants,setAllRestaurants]= useState([]);
-    const[ShawarmaRes, setShawarmaRes] = useState ([]);
-    const[SeaFoodRes, setSeaFoodRes] = useState ([]);
-    const[OrientalFoodRes, setOrientalFoodRes] = useState ([]);
-    const[PizzaRes, setPizzaRes] = useState ([]);
-    const[FriedRes, setFriedRes] = useState ([]);
-    const[FastFoodRes, setFastFoodRes] = useState ([]);
-    useEffect(() => {
-      axios.get("http://localhost:3000/restaurant")
-        .then(res => {
-          setShawarmaRes(res.data.shawarma);
-          setSeaFoodRes(res.data.seafood);
-          setOrientalFoodRes(res.data.orientalfood);
-          setPizzaRes(res.data.pizza);
-          setFriedRes(res.data.fried);
-          setFastFoodRes(res.data.fastfood)
-        })
-        .catch(error => {
-          console.error('Error fetching restaurants:', error);
-        });
-    }, []);
-    
-    useEffect(() => {
-      setAllRestaurants([...ShawarmaRes, ...SeaFoodRes, ...OrientalFoodRes, ...PizzaRes, ...FriedRes]);
-    }, [ShawarmaRes, SeaFoodRes, OrientalFoodRes, PizzaRes, FriedRes]);
-    
+      })
+  }, [])
 
-<<<<<<< HEAD
-const ExchangedData={FastFoodRes,userData, saveUserData,AllRestaurants,ShawarmaRes,SeaFoodRes, OrientalFoodRes, PizzaRes,  FriedRes }
-    return (
-        <DataContext.Provider value={ExchangedData}>
-            {children}
-=======
-    function DeleteUserData() {
-        localStorage.removeItem("userToken")
-        window.location.reload();
-    }
+  const [userData, setUserData] = useState(null);
+  function saveUserData() {
+    let encodedToken = localStorage.getItem("userToken");
+    let decodedToken = jwtDecode(encodedToken);
+    setUserData(decodedToken);
+  }
 
-    return (
-        <DataContext.Provider value={{ userData, saveUserData,movies,DeleteUserData,ImgsArr}}>
-            {props.children}
->>>>>>> 463d078cb651af21a94a7e0518887d58565c9bbd
-        </DataContext.Provider>
-    );
-    
+  function DeleteUserData() {
+    localStorage.removeItem("userToken")
+    window.location.reload();
+  }
+
+  // Restaurant Data Management
+  const [AllRestaurants, setAllRestaurants] = useState([]);
+  const [ShawarmaRes, setShawarmaRes] = useState([]);
+  const [SeaFoodRes, setSeaFoodRes] = useState([]);
+  const [OrientalFoodRes, setOrientalFoodRes] = useState([]);
+  const [PizzaRes, setPizzaRes] = useState([]);
+  const [FriedRes, setFriedRes] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/restaurant")
+      .then(res => {
+        setShawarmaRes(res.data.shawarma);
+        setSeaFoodRes(res.data.seafood);
+        setOrientalFoodRes(res.data.orientalfood);
+        setPizzaRes(res.data.pizza);
+        setFriedRes(res.data.fried);
+        setAllRestaurants([...res.data.shawarma, ...res.data.seafood, ...res.data.orientalfood, ...res.data.pizza, ...res.data.fried]);
+      })
+      .catch(error => {
+        console.error('Error fetching restaurants:', error);
+      });
+  }, []);
+
+  const ExchangedData = {
+    userData,
+    saveUserData,
+    movies,
+    DeleteUserData,
+    ImgsArr,
+    AllRestaurants,
+    ShawarmaRes,
+    SeaFoodRes,
+    OrientalFoodRes,
+    PizzaRes,
+    FriedRes
+  }
+
+  return (
+    <DataContext.Provider value={ExchangedData}>
+      {props.children}
+    </DataContext.Provider>
+  );
 }
